9ff2335f152bbd6ac5861672fcf3113dd0868d0d
yim0327 <yim020327@gmail.com>
2026-08-24 00:51:10 +0900

    fix: Android WebView 배포 URL 변경

--- android/app/src/main/java/com/likelion/tometa/config/ToMetaEndpoint.kt
+++ android/app/src/main/java/com/likelion/tometa/config/ToMetaEndpoint.kt
@@ -1,6 +1,6 @@
 package com.likelion.tometa.config
 
 object ToMetaEndpoint {
-    const val WEB_URL = "https://14th-to-meta-frontend.vercel.app"
+    const val WEB_URL = "https://tometa-final.vercel.app"
     val API_BASE_URL = "$WEB_URL/"
 }
